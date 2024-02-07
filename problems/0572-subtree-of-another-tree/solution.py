f3759cfcbb82547032d85758619ae5714de28f16
k <k108@users.noreply.github.com>
2024-02-07 21:26:20 +0000

    [LeetCode Sync] - Subtree of Another Tree - Runtime - 72 ms (5.49%), Memory - 16.8 MB (100.00%)

---
+++ problems/0572-subtree-of-another-tree/solution.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+        if not subRoot:
+            return True
+
+        if not root:
+            return False
+
+        if self.same_tree(root, subRoot):
+            return True
+        
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def same_tree(self, root, subRoot):
+        if root == None and subRoot == None:
+            return True
+
+        if root and subRoot and root.val == subRoot.val:
+            return self.same_tree(root.left, subRoot.left) and self.same_tree(root.right, subRoot.right)
+            
+        return False
+
+
+        
