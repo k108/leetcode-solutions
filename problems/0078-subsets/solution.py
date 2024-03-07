99b134b95292f8fa2392097fa01b44cfb842d307
k <k108@users.noreply.github.com>
2024-03-07 21:01:50 +0000

    [LeetCode Sync] - Subsets - Runtime - 35 ms (1.52%), Memory - 16.9 MB (100.00%)

---
+++ problems/0078-subsets/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        '''
+        T(n) = O( n * 2^n )
+        '''
+        result = []
+        subset = []
+        def dfs(i):
+            if i >= len(nums):
+                result.append(subset.copy())
+                return
+            
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i+1)
+
+            # decision not to include nums[i]
+            subset.pop()
+            dfs(i+1)
+        
+        dfs(0)
+        return result
