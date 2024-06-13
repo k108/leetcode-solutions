e8f678c989665eb3a91444da9e1798d809cea9b3
k <k108@users.noreply.github.com>
2024-06-13 14:54:37 +0000

    [LeetCode Sync] - LRU Cache - Runtime - 597 ms (6.64%), Memory - 77.8 MB (71.17%)

--- problems/0146-lru-cache/solution.py
+++ problems/0146-lru-cache/solution.py
@@ -1,59 +1,58 @@
 class Node:
     def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.prev = None
-        self.next = None
+        self.key=key
+        self.val=val
+        self.prev=None
+        self.next=None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        # map key to node
         self.cache = {}
-        # left => LRU, right => MRU
+        # LRU
         self.left = Node(0,0)
+        # MRU
         self.right = Node(0,0)
         self.left.next = self.right
         self.right.prev = self.left
 
-    # remove node from list
-    def remove(self, node):
+    # Delete node from Middle
+    def remove(self,node: Node):
         prev_node = node.prev
         next_node = node.next
-
         prev_node.next = next_node
         next_node.prev = prev_node
 
-
-    # insert node at right
-    def insert(self, node):
+    # Insert on MRU
+    def insert(self, node: Node):
         prev_node = self.right.prev
         next_node = self.right
         prev_node.next = next_node.prev = node
-        node.next, node.prev = next_node, prev_node
-        
+        node.next=next_node
+        node.prev=prev_node
 
     def get(self, key: int) -> int:
         if key in self.cache:
+            # remove and insert in DLL
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
         else:
             return -1
-        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
+            # remove and insert in DLL
             self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
+        self.cache[key] = Node(key=key, val=value)
         self.insert(self.cache[key])
-
-        if len(self.cache) > self.capacity:
-            # remove LRU from doubly linked list and remove LRU from hashmap
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
+        
+        if len(self.cache)>self.capacity:
+            lru_node = self.left.next
+            self.remove(lru_node)
+            del self.cache[lru_node.key]
+        
         
 
 
