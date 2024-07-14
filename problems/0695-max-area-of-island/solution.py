e53a1435ab91b3feb6e0b13fe38d68d4ad8c537a
k <k108@users.noreply.github.com>
2024-07-14 23:24:18 +0000

    [LeetCode Sync] - Max Area of Island - Runtime - 115 ms (5.86%), Memory - 18.4 MB (47.92%)

--- problems/0695-max-area-of-island/solution.py
+++ problems/0695-max-area-of-island/solution.py
@@ -1,36 +1,23 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
-
         rows=len(grid)
         cols=len(grid[0])
         visited = set()
         max_area = 0
 
-        def bfs(r, c):
-            area = 0
-            queue = []
+        def dfs(r, c):
+            if (0 > r) or (r == rows) or (0 > c) or (c == cols) or (grid[r][c] == 0) or ((r, c) in visited):
+                return 0
+
             visited.add((r, c))
-            queue.append((r, c))
-            while queue:
-                row, col = queue.pop(0)
-                directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
-                for dr, dc in directions:
-                    r, c = row+dr, col+dc
-                    if (0<=r<rows) and (0<=c<cols) and grid[r][c]==1 and (r,c) not in visited:
-                        area+=1
-                        queue.append((r, c))
-                        visited.add((r, c))
-            return area
+            return (1 + dfs(r+1, c) + \
+                        dfs(r-1, c) + \
+                        dfs(r, c+1) + \
+                        dfs(r, c-1)
+                    )
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]==1 and (r,c) not in visited:
-                    area = 1
-                    area += bfs(r, c)
-                    max_area = max(max_area, area)
+                max_area = max(max_area, dfs(r,c))
 
         return max_area
-
-        
