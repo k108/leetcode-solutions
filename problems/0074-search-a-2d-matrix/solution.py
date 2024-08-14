50be4cb6b2bec36193c02653eb0519fd21d63e6e
k <k108@users.noreply.github.com>
2024-08-14 03:16:45 +0000

    [LeetCode Sync] - Search a 2D Matrix - Runtime - 44 ms (0.18%), Memory - 17.2 MB (99.99%)

--- problems/0074-search-a-2d-matrix/solution.py
+++ problems/0074-search-a-2d-matrix/solution.py
@@ -1,29 +1,27 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # T(n) = O(log(m*n))
-        # S(n) = O(1)
         num_rows = len(matrix)
         num_cols = len(matrix[0])
 
-        def binary_search(matrix, low, high, key):
-            if low > high:
-                return False
-            mid = (low+high) // 2
+        left=0
+        right=(num_rows*num_cols)-1
+
+        while left<=right:
+            mid=(left+right)//2
             # row_no = index_in_flattened_array // num_cols
             # col_no = index_in_flattened_array % num_cols
             # coordinate = (row_no, col_no)
             row = mid // num_cols
             col = mid % num_cols
             guess = matrix[row][col]
-            if guess==key:
+
+            if guess == target:
                 return True
-            elif guess<key:
-                low = mid+1
-                return binary_search(matrix, low, high, key)
+            elif guess < target:
+                left=mid+1
             else:
-                high = mid-1
-                return binary_search(matrix, low, high, key)
+                right=mid-1
         
-        return binary_search(matrix, 0, (num_rows*num_cols)-1, target)
+        return False
 
         
