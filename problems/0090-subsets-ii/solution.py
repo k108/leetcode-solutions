f770b4190a2276e83874bbcaf55bf1fe72d1ca30
k <k108@users.noreply.github.com>
2024-08-18 04:04:26 +0000

    [LeetCode Sync] - Subsets II - Runtime - 37 ms (5.15%), Memory - 16.6 MB (100.00%)

--- problems/0090-subsets-ii/solution.py
+++ problems/0090-subsets-ii/solution.py
@@ -1,22 +1,32 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        '''
+        Time Complexity : O(n * 2^n)
+        '''
+        N = len(nums)
         nums.sort()
+        result = []
+
         def dfs(i, subset):
-            if i >= len(nums):
+            # base case
+            if i >= N:
                 result.append(subset.copy())
                 return
-            
-            # decision to include nums[i]
+
+            # decision to include nums[i], left branch
             subset.append(nums[i])
             dfs(i+1, subset)
 
-            # decision not to include nums[i]
+            # decision NOT to include nums[i], right branch
             subset.pop()
-            while i+1 < len(nums) and nums[i+1] == nums[i]:
+
+            # skip already used element
+            while i+1<N and nums[i+1]==nums[i]:
                 i+=1
+
             dfs(i+1, subset)
-        
+
         dfs(0, [])
         return result
+            
         
