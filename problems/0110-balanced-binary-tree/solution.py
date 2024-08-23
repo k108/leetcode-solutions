694bf2d0c39c6b358d616551e433472343abb134
k <k108@users.noreply.github.com>
2024-08-23 11:12:58 +0000

    [LeetCode Sync] - Balanced Binary Tree - Runtime - 46 ms (5.47%), Memory - 17.7 MB (100.00%)

--- problems/0110-balanced-binary-tree/solution.py
+++ problems/0110-balanced-binary-tree/solution.py
@@ -7,15 +7,25 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         self.height_balanced = True
-        def dfs(root):
-            if not root:
+        # base case
+        # the depth of a non-existent node is 0
+        def dfs(current_node):
+            if not current_node:
                 return 0
-            
-            left_height = dfs(root.left)
-            right_height = dfs(root.right)
-            if abs(left_height - right_height) > 1:
-                self.height_balanced = False
-            return max(left_height, right_height) + 1
+
+            # get the depth of the left side of the tree
+            left_depth = dfs(current_node.left)
+            # get the depth of the right side of the tree
+            right_depth = dfs(current_node.right)
+
+            if abs(left_depth - right_depth) > 1:
+                self.height_balanced=False
+
+            # take the maximum, and +1 for the current node
+            return 1 + max(left_depth, right_depth)
+
         dfs(root)
+        
         return self.height_balanced
+
         
