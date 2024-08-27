0484ffa5c84872120d1b66edf6d4e6c8002f9026
k <k108@users.noreply.github.com>
2024-08-27 09:55:00 +0000

    [LeetCode Sync] - Lowest Common Ancestor of a Binary Search Tree - Runtime - 47 ms (96.75%), Memory - 20.2 MB (99.99%)

--- problems/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
+++ problems/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
@@ -7,17 +7,24 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # If both p and q are smaller than the current node's value,
-        # LCA lies in the left subtree
-        if p.val < root.val and q.val < root.val:
-            return self.lowestCommonAncestor(root.left, p, q)
-        # If both p and q are greater than the current node's value,
-        # LCA lies in the right subtree
-        elif p.val > root.val and q.val > root.val:
-            return self.lowestCommonAncestor(root.right, p, q)
-        # If none of the above conditions satisfy, it means the current node
-        # is the lowest common ancestor
-        else:
+        # There can be 3 cases :
+        # 1. The 1 node is on left of BST and other one is on right -> it means that root is the LCA
+        # 2. Smaller node is less than root -> it means both nodes are on the left, 
+        # so we take root.left as root node and repeat step 1.
+        # 3. Smaller node is greater than root -> it means both nodes are on the right, 
+        # so we take root.right as root node and repeat step 1.
+        if not root:
             return root
+
+        if p.val > q.val:
+            p, q = q, p
+
+        if (p.val <= root.val and q.val >= root.val) or (p.val >= root.val and q.val <= root.val):
+            return root
+        elif p.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return self.lowestCommonAncestor(root.right, p, q)
+
 
         
