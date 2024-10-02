84e75a70c43ffab1d480841658aa09451d474403
k <k108@users.noreply.github.com>
2024-10-02 19:55:23 +0000

    [LeetCode Sync] - Search Insert Position - Runtime - 46 ms (0.67%), Memory - 17.3 MB (100.00%)

---
+++ problems/0035-search-insert-position/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left=0
+        right=len(nums)-1
+
+        while left <= right:
+            mid = (left+right)//2
+            guess = nums[mid]
+            if guess == target:
+                return mid
+            elif guess > target:
+                right = mid-1
+            else:
+                left = mid+1
+
+        return left
+
+        
