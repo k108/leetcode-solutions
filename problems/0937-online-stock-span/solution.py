29b7ee8f2375f6552692e8eef56321b7e72ceb17
k <k108@users.noreply.github.com>
2024-11-30 23:33:44 +0000

    [LeetCode Sync] - Online Stock Span - Runtime - 44 ms (93.32%), Memory - 21.8 MB (88.03%)

--- problems/0937-online-stock-span/solution.py
+++ problems/0937-online-stock-span/solution.py
@@ -1,49 +1,22 @@
-class Stack:
-    def __init__(self):
-        self.items = []
-         
-    # method for pushing an item on a stack
-    def push(self, item):
-        self.items.append(item)
-         
-    # method for popping an item from a stack
-    def pop(self):
-        return self.items.pop()
-     
-    # method to check whether the stack is empty or not
-    def isEmpty(self):
-        return (self.items == [])
-     
-    # method to get the top of the stack
-    def peek(self):
-        return self.items[-1]
-     
-    def __str__(self):
-        return str(self.items)
-
 class StockSpanner:
 
     def __init__(self):
-        self.D = Stack()
-        self.S = []
-        self.A = []
-        self.i = 0
+        """
+        Time Complexity : O(n), as each element will be added or removed from stack atmost once 
+        Space Complexity : O(n), as we are using extra stack
+        """
+        self.stack = [] # pair : (price, span)
         
-
     def next(self, price: int) -> int:
-        self.A.append(price)
-        self.S.append(None)
-        # for i in range (0, len(self.A)):
-        while not self.D.isEmpty() and self.A[self.i] >= self.A[self.D.peek()]:
-                self.D.pop()
-        if self.D.isEmpty():
-            P = -1
-        else:
-            P = self.D.peek()
-        self.S[self.i] = self.i - P
-        self.D.push(self.i)
-        self.i+=1
-        return self.S[-1]
+        span = 1
+        # monotonically decreasing stack
+        while self.stack and self.stack[-1][0] <= price:
+            p, s = self.stack.pop()
+            # we add pre-computed older spans, instead of re-computing them
+            span += s
+        self.stack.append((price, span))
+        return span
+        
         
 
 
