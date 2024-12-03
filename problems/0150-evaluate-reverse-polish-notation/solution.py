8a0aaa5ea3023c80729f4056413fbf6a5e17b14d
k <k108@users.noreply.github.com>
2024-12-03 20:02:02 +0000

    [LeetCode Sync] - Evaluate Reverse Polish Notation - Runtime - 4 ms (30.62%), Memory - 18.8 MB (98.97%)

--- problems/0150-evaluate-reverse-polish-notation/solution.py
+++ problems/0150-evaluate-reverse-polish-notation/solution.py
@@ -1,23 +1,38 @@
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        """
+        Time Complexity : O(n)
+        Space Complexity : O(n), as we are using extra stack
+        """
         # Reverse Polish / Postfix Notation : <Left Operand> - <Right Operand> - <Operator>
+        # if token is not a operator push to stack
+        # if token is a operator pop 2 elements form the stack,
+        # perform the operation and push the result to the stack
         stack = []
         for token in tokens:
-            if token not in ["+", "-", "*", "/"]:
-                stack.append(int(token))
-            else:
+            if token == '+':
                 operand_1 = stack.pop()
                 operand_2 = stack.pop()
-                if token == '+':
-                    stack.append(operand_2 + operand_1)
-                elif token == '-':
-                    stack.append(operand_2 - operand_1)
-                elif token == '*':
-                    stack.append(operand_2 * operand_1)
-                elif token == '/':
-                    stack.append(int(operand_2 / operand_1))
-                # print(operand_2, token, operand_1, "=", stack[-1])
+                stack.append(operand_2 + operand_1)
+            elif token == '-':
+                operand_1 = stack.pop()
+                operand_2 = stack.pop()
+                # order is important operand_2 - operand_1 and not operand_1 - operand_2
+                stack.append(operand_2 - operand_1)
+            elif token == '*':
+                operand_1 = stack.pop()
+                operand_2 = stack.pop()
+                stack.append(operand_2 * operand_1)
+            elif token == '/':
+                operand_1 = stack.pop()
+                operand_2 = stack.pop()
+                # order is important operand_2 / operand_1 and not operand_1 / operand_2
+                # round to zero
+                stack.append(int(operand_2 / operand_1))
+            else:
+                stack.append(int(token))
+            # print(operand_2, token, operand_1, "=", stack[-1])
             
         return stack[0]
 
