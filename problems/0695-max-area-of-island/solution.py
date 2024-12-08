88613b375784f5b149d38288c7a2dbbe307c61a3
k <k108@users.noreply.github.com>
2024-12-08 21:28:29 +0000

    [LeetCode Sync] - Max Area of Island - Runtime - 26 ms (41.47%), Memory - 19 MB (31.62%)

--- problems/0695-max-area-of-island/solution.py
+++ problems/0695-max-area-of-island/solution.py
@@ -1,23 +1,40 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows=len(grid)
-        cols=len(grid[0])
+        if not grid:
+            return 0
+            
+        ROWS = len(grid)
+        COLUMNS = len(grid[0])
         visited = set()
         max_area = 0
 
         def dfs(r, c):
-            if (0 > r) or (r == rows) or (0 > c) or (c == cols) or (grid[r][c] == 0) or ((r, c) in visited):
-                return 0
+            nonlocal area
 
-            visited.add((r, c))
-            return (1 + dfs(r+1, c) + \
-                        dfs(r-1, c) + \
-                        dfs(r, c+1) + \
-                        dfs(r, c-1)
-                    )
+            if 0<=r+1<ROWS and 0<=c<COLUMNS and (r+1,c) not in visited and grid[r+1][c]==1:
+                visited.add((r+1,c))
+                area+=1
+                dfs(r+1, c)
+            if 0<=r-1<ROWS and 0<=c<COLUMNS and (r-1,c) not in visited and grid[r-1][c]==1:
+                visited.add((r-1,c))
+                area+=1
+                dfs(r-1, c)
+            if 0<=r<ROWS and 0<=c+1<COLUMNS and(r,c+1) not in visited and grid[r][c+1]==1:
+                visited.add((r,c+1))
+                area+=1
+                dfs(r, c+1)
+            if 0<=r<ROWS and 0<=c-1<COLUMNS and (r,c-1) not in visited and grid[r][c-1]==1:
+                visited.add((r,c-1))
+                area+=1
+                dfs(r, c-1)
 
-        for r in range(rows):
-            for c in range(cols):
-                max_area = max(max_area, dfs(r,c))
-
+        for r in range(ROWS):
+            for c in range(COLUMNS):
+                if grid[r][c]==1 and (r,c) not in visited:
+                    visited.add((r,c))
+                    area = 1
+                    dfs(r, c)
+                    max_area = max(max_area, area)
+                    
         return max_area
+
