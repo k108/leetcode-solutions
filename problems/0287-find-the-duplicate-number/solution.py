48b6853d13fc21ba7fe5e60cc2a4a5a76d9ed13f
k <k108@users.noreply.github.com>
2025-01-02 04:02:24 +0000

    [LeetCode Sync] - Find the Duplicate Number - Runtime - 160 ms (5.05%), Memory - 29.2 MB (94.36%)

--- problems/0287-find-the-duplicate-number/solution.py
+++ problems/0287-find-the-duplicate-number/solution.py
@@ -1,39 +1,46 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         '''
-        Time Complexity : O(n)
+        Time Complexity : O(n*log(n))
         Time Complexity : O(1)
         '''
 
         '''
         Approaches ->
 
-        1. Floyd's Cycle Detection (Fast-Slow Pointers): Treat the array as a linked list to find a cycle.
-        2. Binary Search: Count the numbers less than or equal to the middle element to find the duplicate.
+        1. Floyd's Cycle Detection (Fast-Slow Pointers): Treat the array as a linked list 
+        to find a cycle.
+
+        2. Binary Search: Count the numbers less than or equal to the middle element 
+        to find the duplicate.
+
+        Pigeonhole principle : if there are more items than containers, 
+        then at least one container must contain more than one item.
+        so as array is [1, n], if 5 is mid, then there should be 5 elements <= 5, if not then we look in [1, mid]; else we search in [mid+1, high]
         '''
 
-        slow = nums[0]
-        fast = nums[0]
+        low = 1
+        high = len(nums) - 1
 
-        # detect cycle :
-        # meeting point is not necessarily the duplicate number; 
-        # it's just a point inside the cycle
-        while True:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-            if slow == fast:
-                break
+        while low < high:
+            mid = (low + high) // 2
 
-        # Find the Start of the Cycle
-        # when both pointers move at the same speed, 
-        # they will eventually meet at the starting point of the cycle
+            count = 0
+            for e in nums:
+                if e<=mid:
+                    count+=1
+            # there are count elements greater than mid, so ans is in [1, mid]
+            if count > mid:
+                high=mid
+            # there are count elements less than equal to mid, so ans is in [mid+1, high]
+            else:
+                low=mid+1
 
-        slow = nums[0]
-        while slow != fast:
-            slow = nums[slow]
-            fast = nums[fast]
-        
-        return slow
+        return low
+
+
+
+
         
 
 
