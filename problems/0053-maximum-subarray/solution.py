ed23119701cd9174581fda8ee334384e1f566f76
k <k108@users.noreply.github.com>
2025-02-14 08:14:33 +0000

    [LeetCode Sync] - Maximum Subarray - Runtime - 79 ms (46.46%), Memory - 32.1 MB (39.30%)

--- problems/0053-maximum-subarray/solution.py
+++ problems/0053-maximum-subarray/solution.py
@@ -1,13 +1,26 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        '''
+        Time Complexity : O(n)
+
+        Space Complexity : O(1)
+        '''
+
+        '''
+        Kadane’s Algorithm :
+        It calculates the maximum sum subarray ending at a particular position 
+        by using the maximum sum subarray ending at the previous position.
+        '''
+        # stores maximum sum so far
         max_sum = nums[0]
+        # stores maximum sum ending here
         curr_sum = 0
 
         for n in nums:
-            curr_sum = max(curr_sum, 0)
             curr_sum+=n
             max_sum = max(max_sum, curr_sum)
+            curr_sum = max(curr_sum, 0)
 
         return max_sum
 
-
+        
