683ba4b4911b5645be6204658045ab6bf5740fb7
k <k108@users.noreply.github.com>
2025-04-27 20:59:51 +0000

    [LeetCode Sync] - Longest Consecutive Sequence - Runtime - 42 ms (87.66%), Memory - 34.3 MB (53.84%)

--- problems/0128-longest-consecutive-sequence/solution.py
+++ problems/0128-longest-consecutive-sequence/solution.py
@@ -1,25 +1,22 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        consecutive_map = {}
-        for num in nums:
-            if num - 1 in consecutive_map:
-                consecutive_map[num-1]=num
-            if num + 1 in consecutive_map:
-                consecutive_map[num]=num+1
-            if num not in consecutive_map:
-                consecutive_map[num]=None
+        '''
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        '''
+        # First turn the input into a set of numbers -> O(n)
+        # Then we can ask in O(1) whether we have a certain number
+        # Since we check each streak only once, this is overall O(n)
+        # Avoids unnecessary backward scans
+
+        # Using hashset
+        nums = set(nums)
         lcs = 0
-        for key in consecutive_map.keys():
-            if consecutive_map[key]==None:
-                counter = 0
-                while key in consecutive_map:
-                    key -= 1
-                    counter+=1
-                lcs = max(lcs, counter)
-
+        for x in nums:
+            # Number x is the start of a streak (i.e., x-1 is not in the set)
+            if x - 1 not in nums:
+                y = x + 1
+                while y in nums:
+                    y += 1
+                lcs = max(lcs, y - x)
         return lcs
-
-
-
-            
-        
