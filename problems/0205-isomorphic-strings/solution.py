3e1959c6fe2348e664ec8e3ebfabd044a5af3d3e
k <k108@users.noreply.github.com>
2025-10-05 21:02:05 +0000

    [LeetCode Sync] - Isomorphic Strings - Runtime - 2 ms (97.19%), Memory - 18.1 MB (31.49%)

--- problems/0205-isomorphic-strings/solution.py
+++ problems/0205-isomorphic-strings/solution.py
@@ -1,20 +1,7 @@
-from collections import defaultdict
-
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         '''
-        Whenever a character appears in s, 
-        it must map to the same character in t as it did before
-        so we track last seen idx
-        0 → means not seen yet
-        i + 1 → real index shifted by 1
+        If number of unique characters are the same 
+        and number of unqiue pairs are the same, we can return true.
         '''
-        last_seen_s, last_seen_t = [0]*200, [0]*200
-        if len(s) != len(t):
-            return False
-        for i in range(len(s)):
-            if last_seen_s[ord(s[i])] != last_seen_t[ord(t[i])]:
-                return False
-            last_seen_s[ord(s[i])] = i + 1
-            last_seen_t[ord(t[i])] = i + 1
-        return True
+        return len(set(s)) == len(set(t)) == len(set(zip(s,t)))
