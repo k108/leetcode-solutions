46ac0d0ae443d8a91d2200269154e044634b80f5
k <k108@users.noreply.github.com>
2025-10-30 04:28:07 +0000

    [LeetCode Sync] - Longest Palindromic Subsequence - Runtime - 1800 ms (7.52%), Memory - 42.7 MB (41.67%)

---
+++ problems/0516-longest-palindromic-subsequence/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        '''
+        Time Complexity : O(n^2)
+        Space Complexity : O(n^2)
+        '''
+        '''
+        This problems can be reduced to finding the LCS between
+        the original string and its reversed form
+        '''
+        dp = [[0] * (len(s) + 1) for _ in range(len(s) + 1)]
+        for i in range(len(s)):
+            for j in range(len(s)):
+                if s[i] == s[len(s) - 1 - j]:
+                    dp[i + 1][j + 1] = dp[i][j] + 1
+                else:
+                    dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
+        return dp[-1][-1]
+        
