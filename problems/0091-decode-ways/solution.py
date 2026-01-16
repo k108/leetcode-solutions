2710e48811e3f4b19339c60606b6d30aef886ccf
k <k108@users.noreply.github.com>
2026-01-16 07:21:24 +0000

    [LeetCode Sync] - Decode Ways - Runtime - 4 ms (8.55%), Memory - 19.4 MB (29.23%)

--- problems/0091-decode-ways/solution.py
+++ problems/0091-decode-ways/solution.py
@@ -1,9 +1,24 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        return self.approach_1(s)
+        return self.approach_2(s)
 
     def approach_2(self, s: str) -> int:
-        pass
+        '''
+        Time complexity: O(n)
+        Space complexity: O(n)
+        '''
+        dp = [-1]*(len(s)+1)
+        dp[len(s)] = 1
+        for i in range(len(s)-1, -1, -1):
+            # substring starting with '0' is not a valid encoding
+            if s[i] == '0':
+                dp[i] = 0
+            else:
+                dp[i] = dp[i+1]
+                if i < len(s)-1 and (s[i]=='1' or (s[i]=='2' and s[i+1]<'7')):
+                    dp[i] += dp[i+2]
+        
+        return dp[0] if len(s)!=0 else 0
     
     def approach_1(self, s: str) -> int:
         '''
