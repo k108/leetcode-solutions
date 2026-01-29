1157718f7d1fb721ae1b3cef13e4059a13993ccc
k <k108@users.noreply.github.com>
2026-01-29 05:42:17 +0000

    [LeetCode Sync] - Combination Sum IV - Runtime - 57 ms (16.01%), Memory - 19.5 MB (17.18%)

--- problems/0377-combination-sum-iv/solution.py
+++ problems/0377-combination-sum-iv/solution.py
@@ -1,6 +1,55 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        return self.approach_2(nums, target)
+        return self.approach_3(nums, target)
+
+    def approach_3(self, nums: List[int], target: int) -> int:
+        '''
+        Time Complexity : O(N * target), the outer loop runs target times, and for each iteration,
+        we potentially check all N numbers in nums
+
+        Space Complexity : O(target), The array dp will have target + 1 elements, 
+        each requiring constant space. 
+        So the overall space complexity is linear in terms of the target value.
+        '''
+        '''
+        Dynamic Programming :
+
+        State Definition :
+        dp(t) =  # combinatons that sum up to t
+
+        Base Case :
+        i=0; dp(0)=1
+        Only one way to make 0 target = 1; i.e. choose nothing
+        dp(target) -> Answer
+
+        Recurrence Relation :
+        dp(t) = Σ dp(t - x) for all x ∈ nums where x ≤ t
+        
+        Boundary Condition :
+
+        Table of Variables :
+        i=0; dp(0) = 1
+        i=1; dp(1) = dp(0) = 1
+        i=2; dp(2) = dp(1) + dp(0) = 2
+        i=3; dp(3) = dp(2) + dp(1) + dp(0) = 4
+        i=4; dp(3) + dp(2) + dp(1) = 7
+        '''
+
+        dp = [0] * (target+1)
+        dp[0] = 1
+
+        nums.sort()
+
+        for t in range(1, target + 1):
+            for num in nums:
+                # if t >= num:
+                #     dp[t] += dp[t - num]
+                if num > t:
+                    break
+
+                dp[t] += dp[t - num]
+        
+        return dp[target]
 
     def approach_2(self, nums: List[int], target: int) -> int:
         '''
@@ -29,8 +78,11 @@ class Solution:
             result = 0
 
             for num in nums:
-                if target >= num:
-                    result += dfs(target - num)
+                # if target >= num:
+                #     result += dfs(target - num)
+                if target < num:
+                    break
+                result += dfs(target - num)
             
             dp[target] = result
             return result
@@ -56,8 +108,11 @@ class Solution:
             result = 0
 
             for num in nums:
-                if target >= num:
-                    result += dfs(target - num)
+                # if target >= num:
+                #     result += dfs(target - num)
+                if target < num:
+                    break
+                result += dfs(target - num)
             
             return result
 
