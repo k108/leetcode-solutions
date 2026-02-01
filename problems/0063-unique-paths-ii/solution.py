7b224eaca29ed9ce9af4d793c85a855b02ffa19e
k <k108@users.noreply.github.com>
2026-02-01 23:59:45 +0000

    [LeetCode Sync] - Unique Paths II - Runtime - 2 ms (25.32%), Memory - 19.8 MB (6.01%)

--- problems/0063-unique-paths-ii/solution.py
+++ problems/0063-unique-paths-ii/solution.py
@@ -1,6 +1,76 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        return self.approach_2(obstacleGrid)
+        return self.approach_3(obstacleGrid)
+
+    def approach_3(self, obstacleGrid: List[List[int]]) -> int:
+        '''
+        Time Complexity : O(R * C)
+        Space Complexity : O(C)
+        '''
+        '''
+        Bottom-Up \ Iterative DP :-
+        We just need to know the previous rows results
+        We just need to know the previous column results
+        Last row and last colum outside 
+        them out of bounds can be thought of 0's
+
+        dp[r][c] = number of unique paths from cell (r, c) to destination
+
+        Transition:
+            If obstacleGrid[r][c] == 1:
+                dp[r][c] = 0
+            Else:
+                dp[r][c] = dp[r][c + 1] + dp[r + 1][c]
+
+        We compute the DP table from bottom-right to top-left.
+        Only the previous row is required at any time, so we use 1D arrays.
+        '''
+        R = len(obstacleGrid)
+        C = len(obstacleGrid[0])
+
+        # prev_row[c] represents dp[r+1][c] (the row below the current row)
+        prev_row = [0] * C
+
+        # Traverse rows from bottom to top
+        for r in range(R -1, -1, -1):
+            # curr_row[c] represents dp[r][c] for the current row
+            curr_row = [0] * C
+
+            # Handle last column,
+            # The destination cell (R-1, C-1) is not always reachable
+            # The last column is not always all 1s
+            # Handle the last column separately because:
+            # - There is no cell to the right
+            # - The value only depends on the cell below
+            # - The destination cell (R-1, C-1) is a special base case
+            if obstacleGrid[r][C - 1] == 1:
+                # Obstacle blocks all paths
+                curr_row[C - 1] = 0
+            else:
+                if r == R - 1:
+                    # Destination cell contributes exactly one valid path
+                    # Destination cell
+                    curr_row[C - 1] = 1
+                else:
+                    # Inherit paths from the cell directly below
+                    curr_row[C - 1] = prev_row[C - 1]
+
+            # Fill the rest of the row from right to left
+            for c in range(C-2, -1, -1):
+                if obstacleGrid[r][c] == 1:
+                    # Obstacle blocks all paths through this cell
+                    curr_row[c] = 0
+                else:
+                    # Sum of paths from:
+                    # - Right neighbor (curr_row[c + 1])
+                    # - Bottom neighbor (prev_row[c])
+                    curr_row[c] = curr_row[c + 1] + prev_row[c]
+
+            # Move current row up to become the "previous row" for next iteration
+            prev_row = curr_row
+
+        # Top-left cell contains the total number of unique paths
+        return prev_row[0]
 
     def approach_2(self, obstacleGrid: List[List[int]]) -> int:
         '''
@@ -9,7 +79,8 @@ class Solution:
         '''
         R = len(obstacleGrid)
         C = len(obstacleGrid[0])
-        def dfs(r, c, R, C, dp):
+        dp = [[0]*C for i in range(R)]
+        def dfs(r, c, R, C):
             # reached right most corner or
             # reached bottom most corner and
             # went out of bounds
@@ -28,10 +99,14 @@ class Solution:
                 return 1
             
             # move either down i.e r+1 or right i.e. c+1
-            dp[r][c] = dfs(r + 1, c, R, C, dp) + dfs(r, c+1, R, C, dp)
+            dp[r][c] = dfs(r + 1, c, R, C) + dfs(r, c+1, R, C)
             return dp[r][c]
 
-        return dfs(0, 0, R, C, [[0]*C for i in range(R)])
+        ans = dfs(0, 0, R, C)
+
+        print(dp)
+
+        return ans
 
     def approach_1(self, obstacleGrid: List[List[int]]) -> int:
         '''
