e93b20811895744e77d831db6a9b91bb88f6867f
k <k108@users.noreply.github.com>
2026-02-19 04:19:04 +0000

    [LeetCode Sync] - Coin Change II - Runtime - 1213 ms (16.13%), Memory - 386.3 MB (5.08%)

---
+++ problems/0518-coin-change-ii/solution.py
@@ -0,0 +1,67 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        return self.approach_2(amount, coins)
+
+    def approach_2(self, amount: int, coins: List[int]) -> int:
+        '''
+        Time Complexity : O(2^amount)
+        Space Complexity : O(amount)
+        '''
+        '''
+        Unbounded Knapsack :
+        profit = 1
+        weight = denomination
+        capacity = amount
+        '''
+        @cache
+        def dfs(i, amount):
+            if amount == 0:
+                return 1
+            if amount < 0:
+                return 0
+            if i == len(coins):
+                return 0
+
+            # Skip item i
+            ways = dfs(i + 1, amount)
+
+            # Include item i
+            new_amount = amount - coins[i]
+            if new_amount >= 0:
+                ways += dfs(i, new_amount)
+
+            return ways
+        
+        return dfs(0, amount)
+    
+    def approach_1(self, amount: int, coins: List[int]) -> int:
+        '''
+        Time Complexity : O(2^amount)
+        Space Complexity : O(amount)
+        '''
+        '''
+        Unbounded Knapsack :
+        profit = 1
+        weight = denomination
+        capacity = amount
+        '''
+
+        def dfs(i, amount):
+            if amount == 0:
+                return 1
+            if amount < 0:
+                return 0
+            if i == len(coins):
+                return 0
+
+            # Skip item i
+            ways = dfs(i + 1, amount)
+
+            # Include item i
+            new_amount = amount - coins[i]
+            if new_amount >= 0:
+                ways += dfs(i, new_amount)
+
+            return ways
+        
+        return dfs(0, amount)
