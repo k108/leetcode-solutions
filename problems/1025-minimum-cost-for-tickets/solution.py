2cbd25bcde66fe21939b9bb1af0eaa8ac1bc2c7c
k <k108@users.noreply.github.com>
2026-02-23 05:50:59 +0000

    [LeetCode Sync] - Minimum Cost For Tickets - Runtime - 0 ms (100.00%), Memory - 20.1 MB (10.97%)

---
+++ problems/1025-minimum-cost-for-tickets/solution.py
@@ -0,0 +1,115 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        return self.approach_2(days, costs)
+
+    def approach_2(self, days: List[int], costs: List[int]) -> int:
+        '''
+        Time Complexity : O(n), where n = 365
+        Space Complexity : O(n), where n = 365
+        '''
+        '''
+        Appoach : Memoization / Unbounded Knapsack
+        Capacity : days
+        Weight : costs
+        Profit : minimum cost
+
+        State : 
+        
+        f(i) = Minimum price for the i to 365 of the days
+
+        Recurrence :
+
+        If day 'i' is a travel day:
+
+        f(i) = min({ 
+				oneDayPass + exceptToday,
+				oneWeekPass + expectThisWeek,
+				oneMonthPass, expectThisMonth 
+			})
+
+        If not a travel day:
+
+        f(i) = f(i+1)
+        '''
+
+        def dfs(i):
+            # reahed end of year
+            if i > 365:
+                return 0
+
+            if dp[i] != -1:
+                return dp[i]
+            
+            # Skip
+            # not travelling today, go to next day
+            # without buying any ticket
+            if i not in days:
+                result = dfs(i+1)
+                dp[i] = result
+                return result
+
+            # Include
+            result = min(
+                costs[0] + dfs(i+1),
+                costs[1] + dfs(i+7),
+                costs[2] + dfs(i+30),
+            )
+            dp[i] = result
+            return result
+
+        days = set(days)
+        dp = [-1]*366
+        
+        return dfs(1)
+
+    def approach_1(self, days: List[int], costs: List[int]) -> int:
+        '''
+        Time Complexity : O(3^n), where n = 365
+        Space Complexity : O(n), where n = 365
+        '''
+        '''
+        Appoach : DFS / Unbounded Knapsack
+        Capacity : days
+        Weight : costs
+        Profit : minimum cost
+
+        State : 
+        
+        f(i) = Minimum price for the i to 365 of the days
+
+        Recurrence :
+
+        If day 'i' is a travel day:
+
+        f(i) = min({ 
+				oneDayPass + exceptToday,
+				oneWeekPass + expectThisWeek,
+				oneMonthPass, expectThisMonth 
+			})
+
+        If not a travel day:
+
+        f(i) = f(i+1)
+        '''
+
+        def dfs(i):
+            # reahed end of year
+            if i > 365:
+                return 0
+            
+            # Skip
+            # not travelling today, go to next day
+            # without buying any ticket
+            if i not in days:
+                return dfs(i+1)
+
+            # Include
+            return min(
+                costs[0] + dfs(i+1),
+                costs[1] + dfs(i+7),
+                costs[2] + dfs(i+30),
+            )
+
+        days = set(days)
+        
+        return dfs(1)
