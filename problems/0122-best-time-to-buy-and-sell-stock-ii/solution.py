c602401dcd50a7b40409b426567ac6b0946cc097
k <k108@users.noreply.github.com>
2026-03-13 08:21:24 +0000

    [LeetCode Sync] - Best Time to Buy and Sell Stock II - Runtime - 7 ms (14.35%), Memory - 20.5 MB (30.70%)

--- problems/0122-best-time-to-buy-and-sell-stock-ii/solution.py
+++ problems/0122-best-time-to-buy-and-sell-stock-ii/solution.py
@@ -1,6 +1,60 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        return self.approach_5(prices)
+        return self.approach_6(prices)
+
+    def approach_6(self, prices: List[int]) -> int:
+        '''
+        Time Complexity : O(N)
+        Space Complexity : O(1)
+        '''
+        '''
+        Approach : State Machine
+
+        State :
+        NOT_HOLD -> we do not own stock (can buy)
+        HOLD -> we own stock (can sell)
+
+        State Transitions :
+        NOT_HOLD -> HOLD      (buy stock)
+        NOT_HOLD -> NOT_HOLD  (do nothing)
+        HOLD -> NOT_HOLD      (sell stock)
+        HOLD -> HOLD          (keep holding)
+
+        State Definitions :
+        hold[i] = max profit on day i while holding stock
+        not_hold[i] = max profit on day i while not holding stock
+
+        hold[i] = max(
+            hold[i-1],             # keep holding
+            not_hold[i-1] - price  # buy today
+            )
+
+        not_hold[i] = max(
+            not_hold[i-1],         # do nothing
+            hold[i-1] + price      # sell today
+            )
+        
+        Initial State :
+        hold = -prices[0] ( can only buy )
+        not_hold = 0
+        '''
+
+        hold = -prices[0]
+        not_hold = 0
+
+        for i in range(1, len(prices)):
+            prev_hold = hold
+            hold = max(
+                hold,             # keep holding
+                not_hold - prices[i]  # buy today
+                )
+
+            not_hold = max(
+                not_hold,         # do nothing
+                prev_hold + prices[i]      # sell today
+                )
+        
+        return not_hold
 
     def approach_5(self, prices: List[int]) -> int:
         '''
@@ -9,6 +63,9 @@ class Solution:
         '''
         '''
         Approach : Greedy
+
+        Every increasing price difference contributes to profit, 
+        and collecting all of them is always optimal.
         '''
         profit = 0
         for i in range(1, len(prices)):
