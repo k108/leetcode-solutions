51c043070ee88bba252d6c5fd1b549786f0a33ae
k <k108@users.noreply.github.com>
2026-03-16 00:07:47 +0000

    [LeetCode Sync] - Best Time to Buy and Sell Stock III - Runtime - 439 ms (26.61%), Memory - 30.9 MB (77.44%)

--- problems/0123-best-time-to-buy-and-sell-stock-iii/solution.py
+++ problems/0123-best-time-to-buy-and-sell-stock-iii/solution.py
@@ -1,6 +1,58 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        return self.approach_2(prices)
+        return self.approach_3(prices)
+
+    def approach_3(self, prices: List[int]) -> int:
+        '''
+        Time Complexity : O(N)
+        Space Complexity : O(1)
+        '''
+        '''
+        Approach : 1-D DP
+
+        curr = dp[i]
+        next = dp[i+1]
+
+        State :
+
+        next[k][0] ->
+
+        Maximum profit starting from the next day when:
+        k transactions left
+        not holding stock
+
+        next[k][1] ->
+
+        Maximum profit starting from the next day when:
+        k transactions left
+        holding stock
+        '''
+
+        # int[][][] dp = new int[3][2]
+        next = [[0]*2 for _ in range(3)]
+
+        for i in range(len(prices)-1, -1, -1):
+            curr = [[0]*2 for _ in range(3)]
+            for k in range(1, 3):
+
+                # buy
+                curr[k][0] = max(
+                    -prices[i] + next[k][1], # buy
+                    next[k][0] # skip
+                )
+
+                # sell
+                curr[k][1] = max(
+                    prices[i] + next[k-1][0], # sell
+                    next[k][1] # skip
+                )
+
+            next = curr
+        
+        # start at day 0
+        # 2 transactions available
+        # not holding stock
+        return next[2][0]
 
     def approach_2(self, prices: List[int]) -> int:
         '''
@@ -17,7 +69,7 @@ class Solution:
         starting at day i
         k transactions remaining
         not holding stock
-        
+
         dp[i][k][1]
 
         Maximum profit:
