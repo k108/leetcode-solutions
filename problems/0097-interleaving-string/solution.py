9548596cedd7aa318ddfe909bb05d9819fe186f8
k <k108@users.noreply.github.com>
2026-03-23 07:37:30 +0000

    [LeetCode Sync] - Interleaving String - Runtime - 52 ms (54.21%), Memory - 19.4 MB (79.57%)

--- problems/0097-interleaving-string/solution.py
+++ problems/0097-interleaving-string/solution.py
@@ -1,16 +1,53 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        return self.approach_2(s1, s2, s3)
+        return self.approach_3(s1, s2, s3)
 
     def approach_3(self, s1: str, s2: str, s3: str) -> bool:
         '''
         Time Complexity : O(len(s1) * len(s2))
-        Space Complexity : O(len(s1) * len(s2))
+        Space Complexity : O(len(s2))
         '''
         '''
         Approach : Bottom-Up DP - 1D DP
+
+        Collapse 2D to 1D
+
+        dp[j] = dp[i][j]
+
+        dp[j] (before update) -> dp[i-1][j] (UP)
+        dp[j-1] (already updated) -> dp[i][j-1] (LEFT)
+
+        dp[j] -> old value -> dp[i-1][j]
+        dp[j-1] -> new value -> dp[i][j-1]
+
         '''
-        pass
+        if len(s1) + len(s2) != len(s3):
+            return False
+
+        dp = [False] * (len(s2) + 1)
+
+        dp[0] = True
+
+        for j in range(1, len(s2) + 1):
+            dp[j] = dp[j-1] and s2[j-1] == s3[j-1]
+
+        for i in range(1, len(s1)+1):
+
+            dp[0] = dp[0] and s1[i-1] == s3[i-1]
+
+            for j in range(1, len(s2)+1):
+                ans1 = False
+                ans2 = False
+
+                if s3[i+j-1] == s1[i-1]:
+                    ans1 = dp[j]
+
+                if s3[i+j-1] == s2[j-1]:
+                    ans2 = dp[j-1]
+
+                dp[j] = ans1 or ans2
+
+        return dp[len(s2)]
 
     def approach_2(self, s1: str, s2: str, s3: str) -> bool:
         '''
