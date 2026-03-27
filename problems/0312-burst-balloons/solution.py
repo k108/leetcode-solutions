aeccf5e2a3c84885b392f6ae888b042d4a30e1e7
k <k108@users.noreply.github.com>
2026-03-27 09:06:30 +0000

    [LeetCode Sync] - Burst Balloons - Runtime - 1755 ms (94.66%), Memory - 22.4 MB (59.44%)

--- problems/0312-burst-balloons/solution.py
+++ problems/0312-burst-balloons/solution.py
@@ -1,6 +1,35 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        return self.approach_1(nums)
+        return self.approach_2(nums)
+
+    def approach_2(self, nums: List[int]) -> int:
+        '''
+        Time Complexity : O(n^3)
+        Space Complexity : O(n^2)
+        '''
+        '''
+        Approach : 2D DP & Interval DP
+        '''
+        nums = [1] + nums + [1]
+
+        dp = [[0]*len(nums) for _ in range(len(nums))]
+
+        # size = distance between left and right
+        for size in range(2, len(nums)):
+            for left in range(len(nums) - size):
+                right = left + size
+
+                result = 0
+
+                for i in range(left+1, right):
+                    coins = nums[left] * nums[i] * nums[right]
+                    left_result = dp[left][i]
+                    right_result = dp[i][right]
+                    result = max(result, coins + left_result + right_result)
+                
+                dp[left][right] = result
+        
+        return dp[0][len(nums)-1]
 
     def approach_1(self, nums: List[int]) -> int:
         '''
