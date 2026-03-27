9dfd4bc056499ffa604a16e7beb90387b685fe85
k <k108@users.noreply.github.com>
2026-03-27 08:25:55 +0000

    [LeetCode Sync] - Burst Balloons - Runtime - 3902 ms (47.20%), Memory - 49.2 MB (5.48%)

---
+++ problems/0312-burst-balloons/solution.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        return self.approach_1(nums)
+
+    def approach_1(self, nums: List[int]) -> int:
+        '''
+        Time Complexity : O(n^3)
+        Space Complexity : O(n^2)
+        '''
+        '''
+        Approach : DFS + Memoization
+
+        State :
+        f(left, right) -> maximum coins that can be collected from (left, right)
+
+        Recurrence :
+        f(left, right) = for i in [left+1, right),
+        max(coins + f(left, i) + f(i, right))
+        '''
+        nums = [1] + nums + [1]
+
+        @cache
+        def dfs(left, right):
+            result = 0
+
+            for i in range(left+1, right):
+                coins = nums[left] * nums[i] * nums[right]
+                left_result = dfs(left, i)
+                right_result = dfs(i, right)
+                result = max(result, coins + left_result + right_result)
+            
+            return result
+        
+        return dfs(0, len(nums)-1)
