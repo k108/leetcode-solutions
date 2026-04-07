ca1ce65bb491f52ff892fb7a97004633621a91d3
k <k108@users.noreply.github.com>
2026-04-07 04:07:14 +0000

    [LeetCode Sync] - Maximal Square - Runtime - 87 ms (53.34%), Memory - 33.1 MB (70.91%)

--- problems/0221-maximal-square/solution.py
+++ problems/0221-maximal-square/solution.py
@@ -1,6 +1,37 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        return self.approach_1(matrix)
+        return self.approach_2(matrix)
+
+    def approach_2(self, matrix: List[List[str]]) -> int:
+        '''
+        Time Complexity : O(R*C)
+        Space Complexity : O(R*C)
+        '''
+        '''
+        Approach : Iterative / Bottom-Up DP
+        '''
+        R = len(matrix)
+        C = len(matrix[0])
+        
+        # Base-case : if r < 0 or c < 0: return 0
+        # dp[0][*] = 0
+        # dp[*][0] = 0
+        dp = [[0] * (C + 1) for _ in range(R + 1)]
+
+        max_side = 0
+
+        for r in range(1, R+1):
+            for c in range(1, C+1):
+                if matrix[r-1][c-1] == '1':
+                    dp[r][c] = min(
+                        dp[r][c-1],     # left
+                        dp[r-1][c],     # top
+                        dp[r-1][c-1]    # diag
+                    ) + 1
+
+                    max_side = max(max_side, dp[r][c])
+        
+        return max_side ** 2
 
     def approach_1(self, matrix: List[List[str]]) -> int:
         '''
@@ -9,9 +40,42 @@ class Solution:
         '''
         '''
         Pick any cell with value 1
-        What is the largest square we can build ending here?
+        What is the largest square we can build ending here? (anywhere in the matrix)
 
         f(i, j) = min(top, left, diag) + 1
+
+        A square ending at (i, j) needs:
+        - A square above
+        - A square to the left
+        - A square diagonally (to ensure interior is filled)
+
+        The smallest of these limits how big your square can be
+
+        We do not use single call to dfs(R-1, C-1) here because it assumes,
+        that the largest square must end at the bottom-right cell, while for
+        this problem it can be anywhere in the matrix
+
+        Recurrence :
+
+        State :
+        f(i, j) = side length of the largest square ending at cell (i, j)
+
+        Recurrence Relation :
+
+        # cannot form any square ending here
+        f(i,j) = 0, if matrix[i][j]=0
+
+        f(i,j) = min(f(i−1,j), f(i,j−1), f(i−1,j−1))+1, if matrix[i][j]=1
+
+        Base Case :
+
+        For i == 0 OR j == 0,
+
+        If the cell is '1':
+        We cannot look up / left / diagonal
+        So the largest square is just the cell itself:
+        f(i, j) = 1
+	​
         '''
 
         R = len(matrix)
