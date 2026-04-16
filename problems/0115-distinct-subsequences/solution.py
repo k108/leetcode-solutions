88a2a2733664f4d552a29ab5e707cef91f0d2241
k <k108@users.noreply.github.com>
2026-04-16 06:14:31 +0000

    [LeetCode Sync] - Distinct Subsequences - Runtime - 422 ms (49.16%), Memory - 75.7 MB (41.77%)

--- problems/0115-distinct-subsequences/solution.py
+++ problems/0115-distinct-subsequences/solution.py
@@ -1,7 +1,41 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        return self.approach_1(s, t)
-    
+        return self.approach_2(s, t)
+
+    def approach_2(self, s: str, t: str) -> int:
+        '''
+        Time Complexity : O(len(s) * len(t))
+        Space Complexity : O(len(s) * len(t))
+        '''
+        '''
+        Approach : Iterative DP
+
+        Base Case :
+        There is exactly 1 way to form an empty string,
+        from any prefix of s -> by deleting everything
+
+        f(i, 0) = 1
+        '''
+        T = len(t)
+        S = len(s)
+
+        # S * T
+        dp = [[0]*(T+1) for _ in range(S+1)]
+
+        # There is exactly 1 way to form an empty string,
+        # from any prefix of s -> by deleting everything
+        for i in range(S+1):
+            dp[i][0] = 1
+        
+        for i in range(1, S+1):
+            for j in range(1, T+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[S][T]
+
     def approach_1(self, s: str, t: str) -> int:
         '''
         Time Complexity : O(len(s) * len(t))
